37d0a2780dd695ef2ece69b2c863e5d9f4cf1e7a
Mohammed Algarni <mgarni@tetco.sa>
2021-02-28 14:13:35 +0300

    PMM PUB v 1.0

---
+++ scripts/processElementData.js
@@ -0,0 +1,80 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_7Emk4QleEeCcxY8AWUWTEAActivity"]="./../../TETCO_PMM/deliveryprocesses/resources/RSL_PM Lifecycle_DC9AFF02_dc9aff02_Activity.jpeg"
+contentPage.processPage.imageFiles["_7Emk4QleEeCcxY8AWUWTEA,_POg3MAoUEeC8rJBuF4yjigActivity"]="./../../TETCO_PMM/deliveryprocesses/resources/Initiating_83DA8EE1_27cdb637_Activity.jpeg"
+contentPage.processPage.imageFiles["_7Emk4QleEeCcxY8AWUWTEA,_CrLdAQoVEeC8rJBuF4yjigActivity"]="./../../TETCO_PMM/deliveryprocesses/resources/Establishing_8527703A_291a9790_Activity.jpeg"
+contentPage.processPage.imageFiles["_7Emk4QleEeCcxY8AWUWTEA,_1y5xACnpEeKKKp96ToB5XAActivity"]="./../../TETCO_PMM/deliveryprocesses/resources/Clos Process implementation_4162F87_a80956dd_Activity.jpeg"
+contentPage.processPage.elementUrls["_kyp5cJ7SEeCMbtrvIADUOw"] = ["Assess and Analyze Process Improvement Opportunities","TETCO_PMM/deliveryprocesses/Assess and Analyze Process Improvement Opportunities_2E377606.html"];
+contentPage.processPage.elementUrls["_1y5xACnpEeKKKp96ToB5XA"] = ["Close-Out Process improvement ","TETCO_PMM/deliveryprocesses/Clos Process implementation_4162F87.html"];
+contentPage.processPage.elementUrls["_f-i-hiqGEeKQlLS_F9s0wA"] = ["Source Control","TETCO_PMM/deliveryprocesses/Source Control_247F72AD.html"];
+contentPage.processPage.elementUrls["_kyqggp7SEeCMbtrvIADUOw"] = ["Business Architecture Document","TETCO_PMM/deliveryprocesses/Business Architecture Document_4C9EE0B9.html"];
+contentPage.processPage.elementUrls["_prtAUCqGEeKQlLS_F9s0wA"] = ["PMO","TETCO_PMM/deliveryprocesses/PMO_402C055E.html"];
+contentPage.processPage.elementUrls["_CXLq4e7yEeSeKMDejhwXcQ"] = ["Project Schedule","TETCO_PMM/deliveryprocesses/Project Schedule_E0CCDACE.html"];
+contentPage.processPage.elementUrls["_f-hwYCqGEeKQlLS_F9s0wA"] = ["Configuration Controller","TETCO_PMM/deliveryprocesses/Configuration Controller_325AAA21.html"];
+contentPage.processPage.elementUrls["_f-i-giqGEeKQlLS_F9s0wA"] = ["Client Workspaces ","TETCO_PMM/deliveryprocesses/Client Workspaces_5FED908E.html"];
+contentPage.processPage.elementUrls["_6asioSqFEeKKJeTPJY715w"] = ["Process improvement Project Statement","TETCO_PMM/deliveryprocesses/Process improvement Project Statement_B4B8A95B.html"];
+contentPage.processPage.elementUrls["_f-i-gCqGEeKQlLS_F9s0wA"] = ["Process improvement Project Statement","TETCO_PMM/deliveryprocesses/Process improvement Project Statement_66B69C68.html"];
+contentPage.processPage.elementUrls["_ikN_euT0EeSF9PvhbmVUew"] = ["Non-Compliance","TETCO_PMM/deliveryprocesses/Non-Compliance_1CF86016.html"];
+contentPage.processPage.elementUrls["_ElG8ciprEeKKJeTPJY715w"] = ["Process Improvement Action Plan","TETCO_PMM/deliveryprocesses/Process Improvement Action Plan_3B9A76E1.html"];
+contentPage.processPage.elementUrls["_k4jdweTtEeSF9PvhbmVUew"] = ["Management Objectives","TETCO_PMM/deliveryprocesses/Management Objectives_751AB128.html"];
+contentPage.processPage.elementUrls["_QbGfkJ7UEeCMbtrvIADUOw"] = ["Lessons Learned","TETCO_PMM/deliveryprocesses/Lessons Learned_7FDBB97D.html"];
+contentPage.processPage.elementUrls["_QbF4gJ7UEeCMbtrvIADUOw"] = ["Establish Process Pilot Implementation","TETCO_PMM/deliveryprocesses/Establish Process Pilot Implementation_5AAB42F0.html"];
+contentPage.processPage.elementUrls["_prsZQCqGEeKQlLS_F9s0wA"] = [" Initiate Process Improvement Project","TETCO_PMM/deliveryprocesses/Initiate Process Improvement Project_5BE6325C.html"];
+contentPage.processPage.elementUrls["_7Emk4QleEeCcxY8AWUWTEA"] = ["RSL_PM Lifecycle","TETCO_PMM/deliveryprocesses/RSL_PM Lifecycle_DC9AFF02.html"];
+contentPage.processPage.elementUrls["_9AmwUOyXEeSeKMDejhwXcQ"] = ["Process Improvement Proposals (PIPs)","TETCO_PMM/deliveryprocesses/Process Improvement Proposals PIPs_263801CC.html"];
+contentPage.processPage.elementUrls["_GZ0n4J7UEeCMbtrvIADUOw"] = ["Business Architecture Document","TETCO_PMM/deliveryprocesses/Business Architecture Document_3C4BFA15.html"];
+contentPage.processPage.elementUrls["_GZ0A0J7UEeCMbtrvIADUOw"] = ["Maintain Process Definition","TETCO_PMM/deliveryprocesses/Maintain Process Definition_31142AC.html"];
+contentPage.processPage.elementUrls["_GZ0A1p7UEeCMbtrvIADUOw"] = ["Process Engineer","TETCO_PMM/deliveryprocesses/Process Engineer_C0DA18F1.html"];
+contentPage.processPage.elementUrls["_ElFuUSprEeKKJeTPJY715w"] = ["PMO","TETCO_PMM/deliveryprocesses/PMO_A6FE79B7.html"];
+contentPage.processPage.elementUrls["_9A1Z0eyXEeSeKMDejhwXcQ"] = ["Management Objectives","TETCO_PMM/deliveryprocesses/Management Objectives_15801646.html"];
+contentPage.processPage.elementUrls["_CXLq4O7yEeSeKMDejhwXcQ"] = ["Schedule and Assign Tasks","TETCO_PMM/deliveryprocesses/Schedule and Assign Tasks_F233C6B8.html"];
+contentPage.processPage.elementUrls["_ElG8cCprEeKKJeTPJY715w"] = ["Lessons Learned","TETCO_PMM/deliveryprocesses/Lessons Learned_426382BB.html"];
+contentPage.processPage.elementUrls["_kyqgiJ7SEeCMbtrvIADUOw"] = ["Process Improvement Action Plan","TETCO_PMM/deliveryprocesses/Process Improvement Action Plan_DC8BB0D1.html"];
+contentPage.processPage.elementUrls["_GZ0n-p7UEeCMbtrvIADUOw"] = ["Process Improvement Action Plan","TETCO_PMM/deliveryprocesses/Process Improvement Action Plan_D585BF62.html"];
+contentPage.processPage.elementUrls["_O0IrtOTqEeSF9PvhbmVUew"] = ["Organizational Risks","TETCO_PMM/deliveryprocesses/Organizational Risks_A5AA2ED9.html"];
+contentPage.processPage.elementUrls["_ikN_ceT0EeSF9PvhbmVUew"] = ["Process Improvement Proposals (PIPs)","TETCO_PMM/deliveryprocesses/Process Improvement Proposals PIPs_8936BBC8.html"];
+contentPage.processPage.elementUrls["_ikN_d-T0EeSF9PvhbmVUew"] = ["QA on QA Report","TETCO_PMM/deliveryprocesses/QA on QA Report_A8A00DAF.html"];
+contentPage.processPage.elementUrls["_CrLdAQoVEeC8rJBuF4yjig"] = ["Establishing and Acting Phase","TETCO_PMM/deliveryprocesses/Establishing_8527703A.html"];
+contentPage.processPage.elementUrls["_3HQ0gCnsEeKKJeTPJY715w"] = ["Process Asset Library","TETCO_PMM/deliveryprocesses/Process Asset Library_4F786616.html"];
+contentPage.processPage.elementUrls["_gvxzwGATEeubCcO1kjmsdw"] = ["Peer Review of PAL and Process Template","TETCO_PMM/deliveryprocesses/Peer Review of PAL and Process Template_3DCE451A.html"];
+contentPage.processPage.elementUrls["_f-i-hCqGEeKQlLS_F9s0wA"] = ["Project Workspaces","TETCO_PMM/deliveryprocesses/Project Workspaces_2B487E87.html"];
+contentPage.processPage.elementUrls["_GZ0n9p7UEeCMbtrvIADUOw"] = ["Process Asset Library","TETCO_PMM/deliveryprocesses/Process Asset Library_C5C58D6.html"];
+contentPage.processPage.elementUrls["_GZ0A1Z7UEeCMbtrvIADUOw"] = ["Obtain Commitment on Process Definition","TETCO_PMM/deliveryprocesses/Obtain Commitment on Process Definition_D24104DB.html"];
+contentPage.processPage.elementUrls["_OL72UCp3EeKKJeTPJY715w"] = ["Close Process Improvement Project","TETCO_PMM/deliveryprocesses/Close Process Improvement Project_C768E8AA.html"];
+contentPage.processPage.elementUrls["_3HQNcCnsEeKKJeTPJY715w"] = ["Perform QA Audit","TETCO_PMM/deliveryprocesses/Perform QA Audit_57285238.html"];
+contentPage.processPage.elementUrls["_4a8p8CnsEeKKJeTPJY715w"] = ["Perform Configuration Audit","TETCO_PMM/deliveryprocesses/Perform Configuration Audit_C5F01796.html"];
+contentPage.processPage.elementUrls["_eNsG8GASEeubCcO1kjmsdw"] = ["Build Process Template ","TETCO_PMM/deliveryprocesses/Build Process Template_FFFFBA84.html"];
+contentPage.processPage.elementUrls["_W69u4SpqEeKKJeTPJY715w"] = ["Configuration Audit Report ","TETCO_PMM/deliveryprocesses/Configuration Audit Report_405C9C.html"];
+contentPage.processPage.elementUrls["_kyp5cp7SEeCMbtrvIADUOw"] = ["Establish Process Improvement Action Plan","TETCO_PMM/deliveryprocesses/Establish Process Improvement Action Plan_276E6A2C.html"];
+contentPage.processPage.elementUrls["_POg3MAoUEeC8rJBuF4yjig"] = ["Initiating and Diagnosing Phase","TETCO_PMM/deliveryprocesses/Initiating_83DA8EE1.html"];
+contentPage.processPage.elementUrls["_f-etECqGEeKQlLS_F9s0wA"] = ["Establish Project’s Environment and CM Plan","TETCO_PMM/deliveryprocesses/Establish Project’s Environment and CM Plan_B7105755.html"];
+contentPage.processPage.elementUrls["_ElG8dCprEeKKJeTPJY715w"] = ["Process Definition Compliance ","TETCO_PMM/deliveryprocesses/Process Definition Compliance_6F564DA.html"];
+contentPage.processPage.elementUrls["_kyp5cZ7SEeCMbtrvIADUOw"] = ["Describe Current Business","TETCO_PMM/deliveryprocesses/Describe Current Business_38D55616.html"];
+contentPage.processPage.elementUrls["_ikN_dOT0EeSF9PvhbmVUew"] = ["Assessment Reports","TETCO_PMM/deliveryprocesses/Assessment Reports_5F2F89D1.html"];
+contentPage.processPage.elementUrls["_kyqggJ7SEeCMbtrvIADUOw"] = ["Process Engineer","TETCO_PMM/deliveryprocesses/Process Engineer_5367EC93.html"];
+contentPage.processPage.elementUrls["_ElFuUCprEeKKJeTPJY715w"] = ["Roll-Out Process Definitions","TETCO_PMM/deliveryprocesses/Roll-Out Process Definitions_9C6099A7.html"];
+contentPage.processPage.elementUrls["_AGIOMdhZEeCnD9RClbyTbQ"] = ["Lessons Learned","TETCO_PMM/deliveryprocesses/Lessons Learned_4B692C65.html"];
+contentPage.processPage.elementUrls["_O0bmoeTqEeSF9PvhbmVUew"] = ["Configuration Management Plan","TETCO_PMM/deliveryprocesses/Configuration Management Plan_57E3DED6.html"];
+contentPage.processPage.elementUrls["_9AvTMeyXEeSeKMDejhwXcQ"] = ["Non-Compliance","TETCO_PMM/deliveryprocesses/Non-Compliance_F6A730DE.html"];
+contentPage.processPage.elementUrls["_O0IrseTqEeSF9PvhbmVUew"] = ["Organizational Measures","TETCO_PMM/deliveryprocesses/Organizational Measures_CFB160D0.html"];
+contentPage.processPage.elementUrls["_ikXwceT0EeSF9PvhbmVUew"] = ["List of PIPs","TETCO_PMM/deliveryprocesses/List of PIPs_DD824596.html"];
+contentPage.processPage.elementUrls["_eQ8PQGASEeubCcO1kjmsdw"] = ["Process Templates","TETCO_PMM/deliveryprocesses/Process Templates_30BAB7F2.html"];
+contentPage.processPage.elementUrls["_GZ1O8J7UEeCMbtrvIADUOw"] = ["Process Definition Compliance ","TETCO_PMM/deliveryprocesses/Process Definition Compliance_4E938291.html"];
+contentPage.processPage.elementUrls["_3HQ0ginsEeKKJeTPJY715w"] = ["QA on QA Report","TETCO_PMM/deliveryprocesses/QA on QA Report_48AF5A3C.html"];
+contentPage.processPage.elementUrls["_f_KCgSqGEeKQlLS_F9s0wA"] = ["Configuration Audit Report ","TETCO_PMM/deliveryprocesses/Configuration Audit Report_6EE1B71E.html"];
+contentPage.processPage.elementUrls["_9AxIYeyXEeSeKMDejhwXcQ"] = ["Configuration Management Plan","TETCO_PMM/deliveryprocesses/Configuration Management Plan_266F43C5.html"];
+contentPage.processPage.elementUrls["_iNa6kCpqEeKKJeTPJY715w"] = ["Baseline Configuration Items","TETCO_PMM/deliveryprocesses/Baseline Configuration Items_6913A4D4.html"];
+contentPage.processPage.elementUrls["_W69H0SpqEeKKJeTPJY715w"] = ["Configuration Controller","TETCO_PMM/deliveryprocesses/Configuration Controller_C705A533.html"];
+contentPage.processPage.elementUrls["_AGDVsdhZEeCnD9RClbyTbQ"] = ["Process Asset Library","TETCO_PMM/deliveryprocesses/Process Asset Library_9290396B.html"];
+contentPage.processPage.elementUrls["_3HQNcSnsEeKKJeTPJY715w"] = ["QA Engineer","TETCO_PMM/deliveryprocesses/QA Engineer_61C63248.html"];
